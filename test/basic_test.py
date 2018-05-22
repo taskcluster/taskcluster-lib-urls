8dcb5c1e90a028d7b218c1ceb4e100ee9843e2e7
Brian Stack <bstack@mozilla.com>
2018-05-22 16:03:59 -0700

    Add python support

---
+++ test/basic_test.py
@@ -0,0 +1,30 @@
+import os
+
+import yaml
+
+import taskcluster_urls as t
+
+
+SPEC_FILE = os.path.join(os.path.dirname(__file__), '..', 'specification.yml')
+OLD_ROOT_URL = 'https://taskcluster.net'
+ROOT_URL = 'https://taskcluster.example.com'
+
+
+def pytest_generate_tests(metafunc):
+    with open(SPEC_FILE) as f:
+        specs = yaml.load(f)['specs']
+        metafunc.parametrize(
+            ['func', 'args', 'expected_url', 'old_expected_url'],
+            [(getattr(t, s['type']),
+                a, s['expectedUrl'], s['oldExpectedUrl'])
+                for s in specs
+                for a in s['argSets']
+             ]
+        )
+
+
+def test_basic(func, args, expected_url, old_expected_url):
+    assert func(ROOT_URL + '/', *args) == expected_url
+    assert func(ROOT_URL, *args) == expected_url
+    assert func(OLD_ROOT_URL + '/', *args) == old_expected_url
+    assert func(OLD_ROOT_URL, *args) == old_expected_url
