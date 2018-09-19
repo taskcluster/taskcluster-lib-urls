7ad09e4c96fe5ff2435c2a17d4d36830fa64bc95
Pete Moore <pmoore@mozilla.com>
2018-09-19 20:40:07 +0200

    Simplified go test code

--- tcurls_test.go
+++ tcurls_test.go
@@ -1,7 +1,6 @@
 package tcurls
 
 import (
-	"fmt"
 	"io/ioutil"
 	"strings"
 	"testing"
@@ -11,36 +10,43 @@ import (
 
 const rootURL = "https://taskcluster.example.com"
 
-type spec struct {
+type Spec struct {
 	FunctionType   string     `yaml:"type"`
 	ExpectedURL    string     `yaml:"expectedUrl"`
 	OldExpectedURL string     `yaml:"oldExpectedUrl"`
 	ArgSets        [][]string `yaml:"argSets"`
 }
 
-type document struct {
-	Specs []spec `yaml:"specs"`
+type Document struct {
+	Specs []Spec `yaml:"specs"`
 }
 
-func testFunc(t *testing.T, functionType string, root string, args ...string) (string, error) {
+func testFunc(t *testing.T, functionType string, expectedURL string, root string, args ...string) {
+	var actualURL string
 	switch functionType {
 	case "api":
-		return API(root, args[0], args[1], args[2]), nil
+		actualURL = API(root, args[0], args[1], args[2])
 	case "apiReference":
-		return APIReference(root, args[0], args[1]), nil
+		actualURL = APIReference(root, args[0], args[1])
 	case "docs":
-		return Docs(root, args[0]), nil
+		actualURL = Docs(root, args[0])
 	case "exchangeReference":
-		return ExchangeReference(root, args[0], args[1]), nil
+		actualURL = ExchangeReference(root, args[0], args[1])
 	case "schema":
-		return Schema(root, args[0], args[1]), nil
+		actualURL = Schema(root, args[0], args[1])
 	case "ui":
-		return UI(root, args[0]), nil
+		actualURL = UI(root, args[0])
 	case "servicesManifest":
-		return ServicesManifest(root), nil
+		actualURL = ServicesManifest(root)
 	default:
-		return "", fmt.Errorf("Unknown function type: %s", functionType)
+		t.Errorf("Unknown function type: %s", functionType)
+		return
 	}
+	if expectedURL != actualURL {
+		t.Errorf(`%v %v(%v) = %q but should be %q`, redCross(), functionType, quotedList(root, args), actualURL, expectedURL)
+		return
+	}
+	t.Logf(`%v %v(%v) = %q`, greenTick(), functionType, quotedList(root, args), actualURL)
 }
 
 func TestURLs(t *testing.T) {
@@ -48,7 +54,7 @@ func TestURLs(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	var specs document
+	var specs Document
 	err = yaml.Unmarshal([]byte(data), &specs)
 	if err != nil {
 		t.Error(err)
@@ -57,42 +63,17 @@ func TestURLs(t *testing.T) {
 	for _, test := range specs.Specs {
 		for _, argSet := range test.ArgSets {
 
-			// Test "new" URLs
-			result, err := testFunc(t, test.FunctionType, rootURL, argSet...)
-			if err != nil {
-				t.Error(err)
-				continue
-			}
-			if result != test.ExpectedURL {
-				t.Errorf("URL is not correct. Got %q wanted %q", result, test.ExpectedURL)
-				continue
-			}
-			result, err = testFunc(t, test.FunctionType, fmt.Sprintf("%s//", rootURL), argSet...)
-			if err != nil {
-				t.Error(err)
-			}
-			if result != test.ExpectedURL {
-				t.Errorf("URL is not correct. Got %q wanted %q", result, test.ExpectedURL)
-				continue
-			}
-			t.Logf(`%v %v(%v) = %q`, greenTick(), test.FunctionType, quotedList(rootURL, argSet), result)
+			// Test "new" URLs without slashes
+			testFunc(t, test.FunctionType, test.ExpectedURL, rootURL, argSet...)
 
-			// Test "old" URLs
-			result, err = testFunc(t, test.FunctionType, oldRootURL, argSet...)
-			if err != nil {
-				t.Error(err)
-			}
-			if result != test.OldExpectedURL {
-				t.Errorf("URL is not correct. Got %q wanted %q", result, test.OldExpectedURL)
-			}
-			result, err = testFunc(t, test.FunctionType, fmt.Sprintf("%s//", oldRootURL), argSet...)
-			if err != nil {
-				t.Error(err)
-			}
-			if result != test.OldExpectedURL {
-				t.Errorf("URL is not correct. Got %q wanted %q", result, test.OldExpectedURL)
-			}
-			t.Logf(`%v %v(%v) = %q`, greenTick(), test.FunctionType, quotedList(oldRootURL, argSet), result)
+			// Test "new" URLs with slashes
+			testFunc(t, test.FunctionType, test.ExpectedURL, rootURL+"//", argSet...)
+
+			// Test "old" URLs without slashes
+			testFunc(t, test.FunctionType, test.OldExpectedURL, oldRootURL, argSet...)
+
+			// Test "old" URLs with slashes
+			testFunc(t, test.FunctionType, test.OldExpectedURL, oldRootURL+"//", argSet...)
 		}
 	}
 }
@@ -107,4 +88,9 @@ func quotedList(url string, args []string) string {
 // green tick (✓) in a color console
 func greenTick() string {
 	return string([]byte{0x1b, 0x5b, 0x33, 0x32, 0x6d, 0xe2, 0x9c, 0x93, 0x1b, 0x5b, 0x30, 0x6d})
+}
+
+// TODO: change this to a red cross
+func redCross() string {
+	return "X"
 }
