a63f0901f1d1233f847ecbe151ea589b9c152247
owlishDeveloper <bugzeeeeee@gmail.com>
2018-10-27 19:08:00 -0700

    Make javascript tests run

--- test/basic_test.js
+++ test/basic_test.js
@@ -7,13 +7,16 @@ const libUrls = require('../');
 const SPEC_FILE = path.join(__dirname, '../tests.yml');
 
 suite('basic test', function() {
+
   var doc = yaml.safeLoad(fs.readFileSync(SPEC_FILE, {encoding: 'utf8'}));
-  for (let test of doc['tests']) {
-    for (let argSet of test['argSets']) {
+  for (let t of doc['tests']) {
+    for (let argSet of t['argSets']) {
       for (let cluster of Object.keys(doc['rootURLs'])) {
         for (let rootURL of doc['rootURLs'][cluster]) {
-          assert.equal(test['expected'][cluster], libUrls.withRootUrl(rootURL)[test['function']](...argSet));
-          assert.equal(test['expected'][cluster], libUrls[test['function']](rootURL, ...argSet));
+          test(`${t['function']} - ${argSet}`, function() {
+            assert.equal(t['expected'][cluster], libUrls.withRootUrl(rootURL)[t['function']](...argSet));
+            assert.equal(t['expected'][cluster], libUrls[t['function']](rootURL, ...argSet));  
+          });
         }
       }
     }
