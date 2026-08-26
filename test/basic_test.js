8a4a5832a66597a9c9a4b84745c2d8a53aa3c0ff
Yaraslau Kurmyza <yarik@mozilla.com>
2026-08-26 13:59:01 +0200

    Apply biome formatting and fixes

--- test/basic_test.js
+++ test/basic_test.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const assert = require('assert');
+const fs = require('node:fs');
+const path = require('node:path');
+const assert = require('node:assert');
 const yaml = require('js-yaml');
 const libUrls = require('../');
 
@@ -8,13 +8,13 @@ const SPEC_FILE = path.join(__dirname, '../tests.yml');
 const TESTS = yaml.load(fs.readFileSync(SPEC_FILE, { encoding: 'utf8' }));
 
 suite('basic test', () => {
-  for (const t of TESTS['tests']) {
-    for (const argSet of t['argSets']) {
-      for (const cluster of Object.keys(TESTS['rootURLs'])) {
-        for (const rootURL of TESTS['rootURLs'][cluster]) {
-          test(`${t['function']} - ${argSet}`, () => {
-            assert.equal(t['expected'][cluster], libUrls.withRootUrl(rootURL)[t['function']](...argSet));
-            assert.equal(t['expected'][cluster], libUrls[t['function']](rootURL, ...argSet));
+  for (const t of TESTS.tests) {
+    for (const argSet of t.argSets) {
+      for (const cluster of Object.keys(TESTS.rootURLs)) {
+        for (const rootURL of TESTS.rootURLs[cluster]) {
+          test(`${t.function} - ${argSet}`, () => {
+            assert.equal(t.expected[cluster], libUrls.withRootUrl(rootURL)[t.function](...argSet));
+            assert.equal(t.expected[cluster], libUrls[t.function](rootURL, ...argSet));
           });
         }
       }
@@ -23,8 +23,8 @@ suite('basic test', () => {
 });
 
 suite('normalization', () => {
-  const correct = TESTS.rootURLs['new'][0];
-  for (const rootUrl of TESTS.rootURLs['new']) {
+  const correct = TESTS.rootURLs.new[0];
+  for (const rootUrl of TESTS.rootURLs.new) {
     test(`normalize ${rootUrl}`, () => {
       assert.equal(libUrls.normalizeRootUrl(rootUrl), correct);
     });
